2a4c236b4344901a880de873eb9f8ba0639e721d
dyst0p <dyst0p.gd@gmail.com>
2021-11-20 14:56:58 +0400

    added movement forward and backward by jumping

--- 1Dungeon/Assets/Scripts/PlayerMovementController.cs
+++ 1Dungeon/Assets/Scripts/PlayerMovementController.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementController : MonoBehaviour
 {
     public float speed;
-    public bool isMotion = false;
+    public bool inMotion = false;
     public BaseCell motionTarget;
+    [SerializeField] private float _transitTime = 3;
+
+
+    public PlayerData Player;
+    public CellsManager Manager;
+
+    public enum MotionDirection { There, Back };
+    public MotionDirection CurrentDirection = MotionDirection.There;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +29,43 @@ public class PlayerMovementController : MonoBehaviour
         
     }
 
-    public void OnMove()
+    public void OnWalkForward()
     {
-        float YInput;
+        if (inMotion)
+            return;
+        if (CurrentDirection == MotionDirection.There)
+            JumpToCell(Manager.GetCellByIndex(GetTargetIndex(MotionDirection.There, 1)));
+        else
+            JumpToCell(Manager.GetCellByIndex(GetTargetIndex(MotionDirection.Back, 1)));
+    }
+
+    public void OnWalkBackward()
+    {
+        if (inMotion)
+            return;
+        if (CurrentDirection == MotionDirection.There)
+            JumpToCell(Manager.GetCellByIndex(GetTargetIndex(MotionDirection.There, -1)));
+        else
+            JumpToCell(Manager.GetCellByIndex(GetTargetIndex(MotionDirection.Back, -1)));
+    }
+
+    public void OnFire()
+    {
+        Debug.Log("Got Message");
+    }
+
+    private int GetTargetIndex(MotionDirection direction, int shift)
+    {
+        int currentIndex = Player.CurrentCell.Index;
+        int finalShift = (direction == MotionDirection.There) ? shift : -shift;
+        return currentIndex + finalShift; // need to add clamp to prevent index out of the bounds
+    }
+
+    private void JumpToCell(BaseCell targetCell)
+    {
+        Player.CurrentCell.Unit = null;
+        targetCell.Unit = Player.gameObject;
+        Player.transform.position = targetCell.transform.position;
+        Player.CurrentCell = targetCell;
     }
 }
